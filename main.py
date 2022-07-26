32241154c2e2f142dcbe920333850da981d34552
Katie McIntosh <katiger@gmail.com>
2022-07-26 03:25:18 +0000

    changed everything

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 import random
 import math
-yesroll = 'yes'
-rolls = []
-while yesroll == 'yes':
+
+#Creating overall rolls array
+overallRolls = []
+
+#creating roll again variable
+again = 'yes'
+
+#Function to add rolls to array
+def arrayadd(array):
+  array.append(diceroll)
+
+#Function to add dice rolls
+def rollsum(array):
+  total = 0
+  for num in array:
+    total += num
+  return total
+
+#Function to average out dice rolls  
+def rollaverage(array):
+  average = rollsum(array)//len(array)
+  return average
+  
+#Rolling the dice
+while again == 'yes':
+  #Creating rolls array
+  rolls = []
+  #Asking how many dice to roll
   diceAmount = input('How many dice would you like to roll? ')
+  #Setting count to zero
   count = 0
   while count < int(diceAmount):
     diceroll = random.randint(1,6)
-    rolls.append(diceroll)
+    #adding to the dice roll count
     count += 1
-    #print (diceroll)
-  else:  
-    #finds the average of the rolls
-    diceSum = math.fsum(rolls)
-    print ('The sum of your rolls is ',diceSum)
-    #finds the index of the rolls list
-    diceAverage = len(rolls)
-    #prints the average of the rolls which is the sum of the rolls divided by the number of rolls.
-    print('The average of your rolls is ', int(diceSum)/int(diceAverage))
-    #lists what the rolls are
-    print ('You rolled ', rolls)
-    #writes the rolls into a text doc
-    f =  open("diceroll.txt","a")
-    f.write(str(rolls))
-    f.close()
-    yesroll = input('Would you like to roll again? ')
+    #adding the rolls to an array
+    arrayadd(rolls)
+    arrayadd(overallRolls)
 
 
+  print('You rolled these dice ',(rolls))
+  print('The sum of your last roll is ',(rollsum(rolls)))
+  print ('The Average of your last roll is ',(rollaverage(rolls)))
+  print('The sum of ALL your rolls is',(rollsum(overallRolls)))
+  print ('The average of ALL your rolls is ',(rollaverage(overallRolls)))
+  print('You have rolled a total of ',str(len(overallRolls)),' times.')
+  f =  open("diceroll.txt","a")
+  f.write('\n You rolled these dice ')
+  f.write(str(rolls))
+  f.write('\n The sum of your rolls was ')
+  f.write(str(rollsum(rolls)))
+  f.write('\n Your average roll was ')
+  f.write(str(rollaverage(rolls)))
+  f.close() 
+  
+  #asking if they would like to roll again
+  again = input('Would you like to roll again? ')
+
+f =  open("total dicerolls.txt","w")
+f.write('\n You have rolled these dice ')
+f.write(str(overallRolls))
+f.write('\n The sum of your all your rolls is ')
+f.write(str(rollsum(overallRolls)))
+f.write('\n Your average roll was ')
+f.write(str(rollaverage(overallRolls)))
+f.close()   
